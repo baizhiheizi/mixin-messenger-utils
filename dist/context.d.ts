63e619af8352295677bb704ff5ddc582a053839a
an-lee <an.lee.work@gmail.com>
2021-02-28 15:47:45 +0800

    feat: add context & share

---
+++ dist/context.d.ts
@@ -0,0 +1,9 @@
+export declare const mixinContext: {
+    appVersion?: string | undefined;
+    immersive?: boolean | undefined;
+    appearance?: "dark" | "light" | undefined;
+    currency?: "USD" | "CNY" | "JPY" | "EUR" | "KRW" | "HKD" | "GBP" | "AUD" | "SGD" | "MYR" | undefined;
+    locale: string;
+    platform: "iOS" | "Android" | "Desktop";
+    conversationId: string;
+};
---
+++ src/share.ts
@@ -0,0 +1,27 @@
+import copy from "copy-to-clipboard";
+import { encode as encode64 } from "js-base64";
+import { mixinContext } from "./context";
+
+export const shareMixinAppCard = (
+  data: {
+    action: string;
+    app_id: string;
+    description: string;
+    icon_url: string;
+    title: string;
+  },
+  success?: () => any,
+  fail?: () => any
+) => {
+  if (mixinContext.platform) {
+    location.replace(
+      `mixin://send?category=app_card&data=${encodeURIComponent(
+        encode64(JSON.stringify(data))
+      )}`
+    );
+    success && success();
+  } else {
+    copy(data.action);
+    fail && fail();
+  }
+};
