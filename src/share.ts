731438e3c020497a1246e3ef25d573692483e4f4
an-lee <an.lee.work@gmail.com>
2021-03-17 09:40:29 +0800

    refactor: share params

--- src/share.ts
+++ src/share.ts
@@ -1,23 +1,24 @@
-import copy from "copy-to-clipboard";
-import { encode as encode64 } from "js-base64";
-import { mixinContext } from "./context";
+import copy from 'copy-to-clipboard';
+import { encode as encode64 } from 'js-base64';
+import { mixinContext } from './context';
 
-export const shareMixinAppCard = (
+export const shareMixinAppCard = (params: {
   data: {
     action: string;
     app_id: string;
     description: string;
     icon_url: string;
     title: string;
-  },
-  success?: () => any,
-  fail?: () => any
-) => {
+  };
+  success?: () => any;
+  fail?: () => any;
+}) => {
+  const { data, success, fail } = params;
   if (mixinContext.platform) {
     location.replace(
       `mixin://send?category=app_card&data=${encodeURIComponent(
-        encode64(JSON.stringify(data))
-      )}`
+        encode64(JSON.stringify(data)),
+      )}`,
     );
     success && success();
   } else {
